ec9e5ef4cb461dcc26b541f18a65e4145e45a2f4
John Internet <jonjon1234.github@gmail.com>
2019-08-13 14:52:59 +0100

    Reformat the tabs to spaces :(

--- workshop/__init__.py
+++ workshop/__init__.py
@@ -10,81 +10,85 @@ env = Env()
 env.read_env()
 
 with env.prefixed("STEAM_"):
-	key = env.str("API_KEY")
-	blacklist = set(env.list("BLACKLIST"))
+    key = env.str("API_KEY")
+    blacklist = set(env.list("BLACKLIST"))
 
-def search(text = "", app = 4000, perpage = 20, cursor = "*"):
-	while cursor:
-		print("Cursor: {}".format(cursor))
-		resp = get(url = "https://api.steampowered.com/IPublishedFileService/QueryFiles/v1/", params = {
-			"key": key,
-			"input_json": dumps({
-				"cursor": cursor,
-				"numperpage": perpage,
-				"creator_appid": app,
-				"appid": app,
-				"search_text": text,
-				"return_children": True,
-				"return_short_description": True,
-				"requiredtags": "Addon",
-				"required_flags": "Addon",
-				"ids_only": False,
-				"return_metadata": True
-			})
-		})
 
-		try:
-			resp = resp.json()['response']
-		except Exception:
-			print(resp.headers)
-			print(resp.text)
-			exit()
+def search(text="", app=4000, perpage=20, cursor="*"):
+    while cursor:
+        print("Cursor: {}".format(cursor))
+        resp = get(url="https://api.steampowered.com/IPublishedFileService/QueryFiles/v1/", params={
+            "key": key,
+            "input_json": dumps({
+                "cursor": cursor,
+                "numperpage": perpage,
+                "creator_appid": app,
+                "appid": app,
+                "search_text": text,
+                "return_children": True,
+                "return_short_description": True,
+                "requiredtags": "Addon",
+                "required_flags": "Addon",
+                "ids_only": False,
+                "return_metadata": True
+            })
+        })
 
-		if not 'publishedfiledetails' in resp:
-			return
+        try:
+            resp = resp.json()['response']
+        except Exception:
+            print(resp.headers)
+            print(resp.text)
+            exit()
 
-		files = [x for x in resp['publishedfiledetails'] if x['result'] == 1]
-		for f in files:
-			yield f
+        if not 'publishedfiledetails' in resp:
+            return
 
-		cursor = resp['next_cursor']
+        files = [x for x in resp['publishedfiledetails'] if x['result'] == 1]
+        for f in files:
+            yield f
+
+        cursor = resp['next_cursor']
+
 
 def query(file):
-	resp = post(url = "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/", data = {
-		"itemcount": 1,
-		"publishedfileids[0]": file
-	})
+    resp = post(url="https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/", data={
+        "itemcount": 1,
+        "publishedfileids[0]": file
+    })
 
-	try:
-		resp = resp.json()['response']
-	except Exception:
-		print(resp.headers)
-		print(resp.text)
-		exit()
+    try:
+        resp = resp.json()['response']
+    except Exception:
+        print(resp.headers)
+        print(resp.text)
+        exit()
 
-	return resp['publishedfiledetails'][0]
+    return resp['publishedfiledetails'][0]
+
 
 def download(url, fi):
-	d = LZMADecompressor()
+    d = LZMADecompressor()
 
-	with get(url) as r:
-		with open("dl.tmp", 'wb') as f:
-			for chunk in r.iter_content(128):
-				if not d.eof:
-					f.write(d.decompress(chunk))
-	rename("dl.tmp", fi)
+    with get(url) as r:
+        with open("dl.tmp", 'wb') as f:
+            for chunk in r.iter_content(128):
+                if not d.eof:
+                    f.write(d.decompress(chunk))
+    rename("dl.tmp", fi)
+
 
 def author(sid):
-	resp = get(url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/", params = {
-		"key": key,
-		"steamids": sid
-	})
+    resp = get(url="https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/", params={
+        "key": key,
+        "steamids": sid
+    })
 
-	try:
-		resp = resp.json()['response']
-	except Exception:
-		print(resp.headers)
-		print(resp.text)
-		exit()
+    try:
+        resp = resp.json()['response']
+    except Exception:
+        print(resp.headers)
+        print(resp.text)
+        exit()
 
-	return resp['players'][0]['personaname']
+    return resp['players'][0]['personaname']
