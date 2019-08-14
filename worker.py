d7587bbea98f71d4f5b22a6d5bf68d2bdac533f6
John Internet <jonjon1234.github@gmail.com>
2019-08-15 00:28:37 +0100

    Fetch last update.

--- worker.py
+++ worker.py
@@ -1,4 +1,5 @@
 from environs import Env
+
 env = Env()
 env.read_env()
 
@@ -7,7 +8,6 @@ import logging
 import workshop
 import mysql.connector
 
-
 logging.error(env.dump())
 logging.basicConfig(level=logging.DEBUG)
 
@@ -15,7 +15,31 @@ logging.basicConfig(level=logging.DEBUG)
 def workshop_update(args):
     wsid = args["wsid"]
     data = workshop.query(wsid)
-    logging.error(data)
+    logging.debug(data)
+
+    con = mysql.connector.connect(
+        user=env.str("MYSQL_USER"),
+        password=env.str("MYSQL_PASSWORD"),
+        host="db",
+        database=env.str("MYSQL_DATABASE")
+    )
+
+    curs = con.cursor()
+    curs.execute('''CREATE TABLE IF NOT EXISTS "addons" ( `wsid` NUMERIC, `name` TEXT, `author` NUMERIC, `lastup` NUMERIC, PRIMARY KEY(`wsid`) )''')
+    curs.execute('''CREATE TABLE IF NOT EXISTS "authors" ( `sid` NUMERIC, `sname` TEXT, PRIMARY KEY(`sid`) )''')
+    curs.execute('''CREATE TABLE IF NOT EXISTS "files" ( `path` TEXT, `owner` NUMERIC )''')
+    curs.execute('''CREATE TABLE IF NOT EXISTS "components" ( `cname` TEXT, `owner` NUMERIC )''')
+    curs.execute('''CREATE TABLE IF NOT EXISTS "cars" ( `cname` TEXT, `owner` NUMERIC )''')
+    curs.execute('''CREATE TABLE IF NOT EXISTS "errors" ( `path` TEXT, `error` TEXT, `owner` NUMERIC )''')
+    curs.close()
+
+    curs = con.cursor(prepared=True)
+    curs.execute("SELECT lastup FROM addons WHERE wsid = %s", (wsid,))
+    lastup = curs.next()
+    logging.debug(lastup)
+
+
+
 
 
 w = Worker()
