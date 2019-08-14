c5d97800ddb641a509c42223560d9dda2440ca18
John Internet <jonjon1234.github@gmail.com>
2019-08-15 00:41:34 +0100

    Wrong quote marks.

--- worker.py
+++ worker.py
@@ -25,12 +25,12 @@ def workshop_update(args):
     )
 
     curs = con.cursor()
-    curs.execute('''CREATE TABLE IF NOT EXISTS "addons" ( `wsid` INTEGER, `name` VARCHAR(500), `author` INTEGER, `lastup` INTEGER, PRIMARY KEY(`wsid`) )''')
-    curs.execute('''CREATE TABLE IF NOT EXISTS "authors" ( `sid` INTEGER, `sname` VARCHAR(500), PRIMARY KEY(`sid`) )''')
-    curs.execute('''CREATE TABLE IF NOT EXISTS "files" ( `path` VARCHAR(500), `owner` INTEGER )''')
-    curs.execute('''CREATE TABLE IF NOT EXISTS "components" ( `cname` VARCHAR(500), `owner` INTEGER )''')
-    curs.execute('''CREATE TABLE IF NOT EXISTS "cars" ( `cname` VARCHAR(500), `owner` INTEGER )''')
-    curs.execute('''CREATE TABLE IF NOT EXISTS "errors" ( `path` VARCHAR(500), `error` VARCHAR(500), `owner` INTEGER )''')
+    curs.execute('''CREATE TABLE IF NOT EXISTS `addons` ( `wsid` INTEGER, `name` VARCHAR(500), `author` INTEGER, `lastup` INTEGER, PRIMARY KEY(`wsid`) )''')
+    curs.execute('''CREATE TABLE IF NOT EXISTS `authors` ( `sid` INTEGER, `sname` VARCHAR(500), PRIMARY KEY(`sid`) )''')
+    curs.execute('''CREATE TABLE IF NOT EXISTS `files` ( `path` VARCHAR(500), `owner` INTEGER )''')
+    curs.execute('''CREATE TABLE IF NOT EXISTS `components` ( `cname` VARCHAR(500), `owner` INTEGER )''')
+    curs.execute('''CREATE TABLE IF NOT EXISTS `cars` ( `cname` VARCHAR(500), `owner` INTEGER )''')
+    curs.execute('''CREATE TABLE IF NOT EXISTS `errors` ( `path` VARCHAR(500), `error` VARCHAR(500), `owner` INTEGER )''')
     curs.close()
 
     curs = con.cursor(prepared=True)
