a0442ab064a95bef54e1765a903e2d26179faf00
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2022-08-05 17:16:41 +0100

    feat: Only print, depending on what we're interested in.

--- lua/photon/emvu.lua
+++ lua/photon/emvu.lua
@@ -2,5 +2,11 @@ EMVU = {}
 EMVU.Configurations = {}
 EMVU.Configurations.Supported = {}
 
-function EMVU:AddAutoComponent(t, n) print(n) end
-function EMVU:OverwriteIndex(n, t) print(n) end
+if VEHICLES then
+    function EMVU:OverwriteIndex(n, t) print(n) end
+    EMVU.AddAutoComponent = function() end
+elseif COMPONENTS then
+    EMVU.OverwriteIndex = function() end
+    function EMVU:AddAutoComponent(t, n) print(n) end
+end
+
